bc558bc768d13a3eca7f27e6262adf3f759feafe
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-29 15:32:32 +0200

    Add product item component w/ logic

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Product } from '../types/types';
+import styled from 'styled-components';
+
+const ProductItemContainer = styled.div`
+  border: 1px solid #ccc;
+  padding: 16px;
+  margin: 16px;
+  max-width: 300px;
+`;
+
+interface ProductItemProps {
+  product: Product;
+}
+
+const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
+  return (
+    <ProductItemContainer>
+      <img src={product.img} alt={product.name} />
+      <h2>{product.name}</h2>
+      <p>{product.objectDesc}</p>
+      <p>Price: ${product.price}</p>
+    </ProductItemContainer>
+  );
+};
+
+export default ProductItem;
