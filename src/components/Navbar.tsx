e561f84f139414a596814c3634f5ee1bc8226c0f
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-09-29 20:33:55 +0200

    Add navbar + initial styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,76 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import cartImage from "/images/Cart.svg"
 
-const NavContainer = styled.nav`
-  background-color: #333;
-  color: #fff;
-  padding: 16px;
+const StyledContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  gap: 25px;
 `;
 
-const NavLink = styled(Link)`
+const StyledNavbar = styled.nav`
+  
+`;
+
+const StyledNavLink = styled(Link)`
+  color: red;
   text-decoration: none;
-  color: #fff;
-  margin-right: 16px;
 
   &:hover {
+    color: #ff0000; 
     text-decoration: underline;
   }
 `;
 
+const StyledButton = styled.button`
+  padding: 0;
+  margin: 0;
+  border: .5px solid black;
+  border-radius: 50%;
+  color: none;
+  background-color: transparent;
+  cursor: pointer;
+  background-image: url(${cartImage});
+  background-size: cover;
+  width: 34px;
+  height: 34px;
+  position: relative;
+`;
+
+const StyledNumberIndicator = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #F9CB00;
+  color: black;
+  border-radius: 50%;
+  font-size: 7px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  transform: translate(25%, 25%);
+  height: 14px;
+  width: 14px;
+`;
+
 const NavBar: React.FC = () => {
   return (
-    <NavContainer>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-    </NavContainer>
+    <StyledContainer>
+        <StyledNavbar>
+          <StyledNavLink to="/">
+            Home
+          </StyledNavLink>
+        </StyledNavbar>
+        <StyledContainer>
+          <StyledNavLink to="/about">
+            About
+          </StyledNavLink>
+          <StyledButton>
+            <StyledNumberIndicator>7</StyledNumberIndicator>
+          </StyledButton>
+        </StyledContainer>
+      </StyledContainer>
   );
 };
 
