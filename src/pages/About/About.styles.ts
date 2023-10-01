c9551632a1a6760de065bab12a175afcacab92c2
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-01 22:04:00 +0200

    Add media query breakpoints to About page

--- src/pages/About/About.styles.ts
+++ src/pages/About/About.styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledMainContainer = styled.div`
   height: 100vh;
-  margin: 5em 4em;
+  margin: 5em 2em;
   font-family: "Courier", sans-serif;
   font-size: 12px;
   line-height: 1.5em;
@@ -11,25 +11,50 @@ export const StyledMainContainer = styled.div`
   align-items: center;
   margin-top: -2rem;
   flex-wrap: wrap;
+
+  @media (max-width: 992px) {
+    margin-bottom: -3rem;
+    margin-top: 3em;
+  }
 `;
 
 export const StyledLeftContainer = styled.div`
   width: 50%;
+
+  @media (max-width: 992px) {
+    width: 100%;
+  }
 `;
 
 export const StyledParagraph = styled.p`
-  margin: 0 10em;
+  @media (min-width: 992px) {
+    margin: 0 10em;
+  }
 `;
 
 export const StyledRightContainer = styled.div`
   width: 50%;
+
+  @media (max-width: 992px) {
+    width: 100%;
+  }
 `;
 
 export const StyledImageContainer = styled.div`
   height: 100%;
-`;
 
-export const StyledImage1 = styled.img`
-  margin-bottom: 13em;
-  padding-right: 3.5em;
-`;
+  @media (max-width: 992px) {
+    display: none;
+  }
+  `;
+  
+  export const StyledImage1 = styled.img`
+    margin-bottom: 13em;
+    padding-right: 3.5em;
+  `;
+  
+  // @media (max-width: 992px) {
+  //   margin-top: 3em;
+  //   display: flex;
+  //   justify-content: center;
+  // }
