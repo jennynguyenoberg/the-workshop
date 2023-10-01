10eec656198fd28129ea717fb696eef59a8387bf
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-10-01 16:49:44 +0200

    Add button components to products

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: transparent;
+  color: black;
+  margin-top: 1em;
+  margin-right: -5px;
+  padding: 5px 15px;
+  border: 0.5px solid black;
+  cursor: pointer;
+  border-radius: 20px;
+  width 20px;
+  font-family: 'Courier', sans-serif;
+  font-size: 12px;
+
+  &:hover {
+    background-color: black;
+    color: #F8F6ED;
+  }
+`;
+
+interface ButtonProps {
+  onClick?: () => void;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
+};
+
+export default Button;
